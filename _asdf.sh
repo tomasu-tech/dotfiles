aeefe4564a3c7f67635306f1139a5d225d92d9ba
tom <55193250+tomlle@users.noreply.github.com>
2022-06-20 14:31:21 +0900

    Update _asdf.sh
    誤字修正

--- _asdf.sh
+++ _asdf.sh
@@ -2,7 +2,7 @@
 
 #install
 which asdf >/dev/null 2>&1 && asdf plugin add ruby && asdf install ruby latest
-which asdf >/dev/null 2>&1 && asdf plugin add python && asdf install python lateest
+which asdf >/dev/null 2>&1 && asdf plugin add python && asdf install python latest
 which asdf >/dev/null 2>&1 && asdf plugin add nodejs && asdf install nodejs latest
 which asdf >/dev/null 2>&1 && asdf plugin add php && asdf install php latest
 
